5bfbcfb1e6aeca4a8be0f3a78a079520474e2833
propaan <legrandenzo.pro@gmail.com>
2023-03-16 11:43:01 +0100

    first commit

---
+++ connect4/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace connect4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox
+            // 
+            this.picBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBox.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.picBox.Location = new System.Drawing.Point(106, 11);
+            this.picBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(523, 511);
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(722, 600);
+            this.Controls.Add(this.picBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox;
+    }
+}
+
---
+++ connect4/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+
+namespace connect4
+{
+    public partial class Form1 : Form
+    {
+
+        private BoardGame jeu;
+        int color = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            jeu = new BoardGame(8, 8);
+            jeu.ShowGrid(picBox);
+
+            picBox.MouseClick += new MouseEventHandler(pictureBox_MouseClick);
+        }
+
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            int colToPlace = e.Location.X / jeu.CellSize;
+
+            char playerColor = (color % 2 == 0) ? 'y' : 'r';
+            bool placed = jeu.PlacePion(colToPlace, playerColor);
+            jeu.ShowGrid(picBox);
+            if (placed)
+            {
+                color++;
+            } else
+            {
+                MessageBox.Show("Le pion ne peut être placé ! ");
+            }
+        }
+
+        class BoardGame
+        {
+            private char[,] board = new char[100, 100];
+            private int row, col;
+            private readonly SolidBrush yellowTeam = new SolidBrush(Color.Yellow);
+            private readonly SolidBrush redTeam = new SolidBrush(Color.Red);
+            private readonly int cellSize = 60;
+            private readonly Pen pen = new Pen(Color.Black, 1);
+
+            public int CellSize
+            {
+                get { return cellSize; }
+            }
+
+            public BoardGame(int nbRow, int nbCols)
+            {
+                this.row = nbRow;
+                this.col = nbCols;
+            }
+
+            public void ShowGrid(PictureBox pictureBox)
+            {
+                Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+                Graphics graphics = Graphics.FromImage(bitmap);
+
+                pictureBox.Height = cellSize * row;
+                pictureBox.Width = cellSize * col;
+
+                for (int x = 0; x < col; x++)
+                {
+                    graphics.DrawLine(pen, x * cellSize, 0, x * cellSize, row * cellSize);
+                    for (int y = 0; y < row; y++)
+                    {
+                        graphics.DrawLine(pen, 0, y * cellSize, col * cellSize, y * cellSize);
+                        int xPos = (x) * cellSize + cellSize / 2;
+                        int yPos = (y) * cellSize + cellSize / 2;
+                        int radius = cellSize / 2;
+
+                        graphics.DrawEllipse(pen, xPos - radius, yPos - radius, cellSize, cellSize);
+
+                        if (board[y, x] == 'r')
+                        {
+                            graphics.FillEllipse(redTeam, xPos - radius, yPos - radius, cellSize, cellSize);
+                        }
+                        else if (board[y, x] == 'y')
+                        {
+                            graphics.FillEllipse(yellowTeam, xPos - radius, yPos - radius, cellSize, cellSize);
+                        }
+                    }
+                }
+
+                pictureBox.Image = bitmap;
+            }
+
+            public bool PlacePion(int colToPlace, char color)
+            {
+                bool success = false;
+                for (int y = 0; y < row; y++)
+                {
+                    if (board[y, colToPlace] == 'r' || board[y, colToPlace] == 'y')
+                    {
+                        if (y == 0)
+                        {
+                            if (board[0, colToPlace] == 'r' || board[0, colToPlace] == 'y')
+                            {
+                                success = false;
+                                break;
+                            }
+                                
+                            this.board[0, colToPlace] = color;
+                            success = true;
+                            break;
+                        } else
+                        {
+                            this.board[y - 1, colToPlace] = color;
+                            success = true;
+                            break;
+                        }
+                    }
+                    else if (y == row - 1)
+                    {
+                        this.board[row - 1, colToPlace] = color;
+                        success = true;
+                        break;
+                    }
+                }
+
+                return success;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
